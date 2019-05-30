01b355254ad1c61320907fcdeaeae9b5679c1c4b
George Ruiz <georuiz817@gmail.com>
2019-05-30 01:54:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,7 +24,8 @@ end
  
 
 def find_the_cheese(cheddar_cheese)
- cheddar_cheese.include?("cheddar")
+ cheddar_cheese.include?(cheese_types)
+   
 
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
